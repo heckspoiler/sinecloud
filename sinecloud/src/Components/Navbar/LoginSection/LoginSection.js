3c817fcaa63820a3871784b208b36cd937a3cb8d
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-06-04 22:11:50 +0200

    login section finished (visually)

---
+++ sinecloud/src/Components/Navbar/LoginSection/LoginSection.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./LoginSection.css";
+
+export const LoginSection = () => {
+  return (
+    <section className="login-section">
+      <button className="login-button">Login</button>
+      <button className="signup-button">SIGN UP</button>
+    </section>
+  );
+};
--- sinecloud/src/Components/Navbar/Navbar.js
+++ sinecloud/src/Components/Navbar/Navbar.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./Navbar.css";
 import { NavbarItems } from "./NavbarItems/NavbarItems";
 import { SearchBar } from "./SearchBar/SearchBar";
+import { LoginSection } from "./LoginSection/LoginSection";
 
 export const Navbar = () => {
   return (
     <nav className="navbar">
       <NavbarItems />
       <SearchBar />
+      <LoginSection />
     </nav>
   );
 };
