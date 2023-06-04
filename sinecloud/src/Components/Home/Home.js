8b507d561e128a08a689e532e864c5e5b54f4964
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-06-05 00:18:20 +0200

    adding main title

--- sinecloud/src/Components/Home/Home.js
+++ sinecloud/src/Components/Home/Home.js
@@ -3,4 +3,10 @@ import "./Home.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
-export const Home = () => {};
+export const Home = () => {
+  return (
+    <section>
+      <h1>sinecloud</h1>
+    </section>
+  );
+};
