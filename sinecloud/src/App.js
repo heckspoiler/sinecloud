18b3d442eb2c0f418feec688555e1ca22d4d4c5c
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-06-04 20:33:19 +0200

    changes

--- sinecloud/src/App.js
+++ sinecloud/src/App.js
@@ -1,8 +1,27 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Link } from "react-router-dom";
+import { motion } from "framer-motion";
+// import { Home } from "./Components/Home/Home";
+import { Navbar } from "./Components/Navbar/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+    </div>
+  );
 }
 
 export default App;
---
+++ sinecloud/src/Components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Navbar.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+export const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+      </ul>
+    </nav>
+  );
+};
