8d18831c87dff9654e0243bbaa6522916630a82d
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-06-04 17:38:01 +0200

    testing out gradient options

--- sinecloud/src/Components/Navbar/Navbar.js
+++ sinecloud/src/Components/Navbar/Navbar.js
@@ -2,14 +2,5 @@ import React, { useState, useRef } from "react";
 import "./Navbar.css";
 
 export const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <li>
-        <a>HOME</a>
-      </li>
-      <li>
-        <a>STATIONS</a>
-      </li>
-    </nav>
-  );
+  return <nav className="navbar"></nav>;
 };
