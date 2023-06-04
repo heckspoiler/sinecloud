cfc77977de272dd41e6eef0a257fe1585b61cb14
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-06-04 21:50:53 +0200

    searchbar finished

--- sinecloud/src/Components/Navbar/Navbar.js
+++ sinecloud/src/Components/Navbar/Navbar.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Navbar.css";
 import { NavbarItems } from "./NavbarItems/NavbarItems";
+import { SearchBar } from "./SearchBar/SearchBar";
 
 export const Navbar = () => {
   return (
     <nav className="navbar">
       <NavbarItems />
+      <SearchBar />
     </nav>
   );
 };
