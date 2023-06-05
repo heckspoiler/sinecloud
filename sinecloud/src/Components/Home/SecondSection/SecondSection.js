89b0d82fb3e4bae727dc70023ead9b6274014d39
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-06-05 22:09:27 +0200

    adding changes

--- sinecloud/src/Components/Home/SecondSection/SecondSection.js
+++ sinecloud/src/Components/Home/SecondSection/SecondSection.js
@@ -2,5 +2,17 @@ import React from "react";
 import "./SecondSection.css";
 
 export const SecondSection = () => {
-  return <section className="second-section"></section>;
+  return (
+    <section className="second-section">
+      <section className="text-section">
+        <h2 className="second-section-title">discover new music</h2>
+        <div className="second-section_block"></div>
+        <p>
+          Browse through the best radio stations out there and expand your
+          musical horizon with us
+        </p>
+      </section>
+      <h2>and much more</h2>
+    </section>
+  );
 };
