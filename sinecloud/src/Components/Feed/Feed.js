9ce231882dffb6b778afbc6d2873b8c482982d02
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-06-20 13:13:49 +0200

    adding backend

--- sinecloud/src/Components/Feed/Feed.js
+++ sinecloud/src/Components/Feed/Feed.js
@@ -6,30 +6,6 @@ export const Feed = () => {
   useEffect(() => {
     document.body.style.backgroundColor = "#3448A8";
 
-    const fetchData = async () => {
-      const options = {
-        method: "GET",
-        url: "https://soundcloud4.p.rapidapi.com/user/info",
-        params: {
-          profile_url: "https://soundcloud.com/kioskradio",
-        },
-        headers: {
-          "X-RapidAPI-Key":
-            "439b27de4amshb8504f8f9c5e92ap1c69f7jsn6ee0a06dd329",
-          "X-RapidAPI-Host": "soundcloud4.p.rapidapi.com",
-        },
-      };
-
-      try {
-        const response = await axios.request(options);
-        console.log(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchData();
-
     return () => {
       document.body.style.backgroundColor = "transparent";
     };
