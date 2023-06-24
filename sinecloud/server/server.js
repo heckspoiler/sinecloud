eb49e81438316f55d3b419dd3f1238e888a103b5
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-06-24 22:49:02 +0200

    created server & starting api fetch backend

--- sinecloud/server/server.js
+++ sinecloud/server/server.js
@@ -1,41 +1,27 @@
-// server.js
-const cors = require("cors");
-require("dotenv").config();
-console.log(process.env.REACT_APP_RAPIDAPI_KEY);
-console.log(process.env.REACT_APP_RAPIDAPI_HOST);
-console.log("Hello from server.js");
 const express = require("express");
-const axios = require("axios");
+const cors = require("cors");
 const app = express();
+
+const PORT = process.env.PORT || 4000;
 
 app.use(
   cors({
     origin: "http://localhost:3000",
-    methods: "GET",
+    methods: "GET, POST",
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
-app.get("/api/feed", async (req, res) => {
-  try {
-    const response = await axios.get(
-      "https://soundcloud4.p.rapidapi.com/user/info",
-      {
-        headers: {
-          "x-rapidapi-key": process.env.REACT_APP_RAPIDAPI_KEY,
-          "x-rapidapi-host": process.env.REACT_APP_RAPIDAPI_HOST,
-        },
-      }
-    );
+app.get("/api/soundcloud", (req, res) => {});
 
-    res.json(response.data);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+app.get("/", (req, res) => {
+  console.log(`Server is listening on ${PORT}`);
+  res.send(
+    "Welcome to the server öjasdföajdflökjadsöflkjasödflkajsdfölkajfölkasjdf"
+  );
 });
 
-const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const test = "test";
-
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log("Port's working wtf");
+  console.log(`Server is listening on ${PORT}, coming from the server side`);
+});
